5d9da1114364cf429ed4eaa6060132f3d7a378c6
Swebus <sebastian.linden16@gmail.com>
2025-10-22 15:00:25 +0200

    add: Docker files & github workflows

--- src/Program.cs
+++ src/Program.cs
@@ -5,7 +5,8 @@ using Microsoft.IdentityModel.Tokens;
 using ShwubbApi;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://localhost:3000");
+builder.WebHost.UseUrls("http://0.0.0.0:3000");
+
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
